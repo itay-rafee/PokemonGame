36fa200a0de16ebf839ce2f13a30f312e6b462fb
Itay.R <itayrafee@gmail.com>
2020-11-23 14:30:19 +0200

    add empty test

---
+++ tests/api/DWGraph_AlgoTest.java
@@ -0,0 +1,39 @@
+package api;
+
+import org.junit.jupiter.api.Test;
+
+
+class DWGraph_AlgoTest {
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void getGraph() {
+    }
+
+    @Test
+    void copy() {
+    }
+
+    @Test
+    void isConnected() {
+    }
+
+    @Test
+    void shortestPathDist() {
+    }
+
+    @Test
+    void shortestPath() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void load() {
+    }
+}
---
+++ tests/api/DWGraph_DSTest.java
@@ -0,0 +1,51 @@
+package api;
+
+import org.junit.jupiter.api.Test;
+
+
+class DWGraph_DSTest {
+
+    @Test
+    void getNode() {
+    }
+
+    @Test
+    void getEdge() {
+    }
+
+    @Test
+    void addNode() {
+    }
+
+    @Test
+    void connect() {
+    }
+
+    @Test
+    void getV() {
+    }
+
+    @Test
+    void getE() {
+    }
+
+    @Test
+    void removeNode() {
+    }
+
+    @Test
+    void removeEdge() {
+    }
+
+    @Test
+    void nodeSize() {
+    }
+
+    @Test
+    void edgeSize() {
+    }
+
+    @Test
+    void getMC() {
+    }
+}
